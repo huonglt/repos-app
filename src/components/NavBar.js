1ebfa01759b1708a2591e5fb502501da1d95dd0e
helen.thu.huong <>
2022-03-10 23:13:15 +1300

    styling for active navbar item

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const NavBar = () => {
+  const [activeItem, setActiveItem] = useState('repos');
+
+  const classNameRepos = `${activeItem === 'repos' && 'navbar-active-item'} navbar-item`;
+  const classNameSettings = `${activeItem === 'settings' && 'navbar-active-item'} navbar-item`;
+
   return (
     <div className="navbar-container">
-      <div className="navbar-item">Repositories</div>
-      <div className="navbar-item">Settings</div>
+      <div className={classNameRepos}>Repositories</div>
+      <div className={classNameSettings}>Settings</div>
     </div>
   ) ;
 };
