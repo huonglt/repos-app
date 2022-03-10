406cb58ca49178183eefe2fa1219c39d2385b90b
helen.thu.huong <>
2022-03-10 23:07:31 +1300

    styling the NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className="navbar-container">
+      <div className="navbar-item">Repositories</div>
+      <div className="navbar-item">Settings</div>
+    </div>
+  ) ;
+};
+
+export default NavBar;
