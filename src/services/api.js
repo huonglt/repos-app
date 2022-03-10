4d899409db83849c79a0b3f37b279c264428a68a
helen.thu.huong <>
2022-03-10 22:11:55 +1300

    extract the const out of method getRepos for unit test

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,17 @@
+export const QUERY_REPOS_URL = 'https://api.github.com/search/repositories?q=org:vacasaoss&sort=stars&order=desc';
+export const REQUEST_HEADER = {
+  'Accept': 'application/vnd.github.v3+json'
+};
+
 /**
  * Method to query all repos for vacasaoss sorting by starrs desending order
  * @returns {Object}
  */
 export const getRepos = async () => {
-  const response = await fetch('https://api.github.com/search/repositories?q=org:vacasaoss&sort=stars&order=desc', {
+  const response = await fetch(QUERY_REPOS_URL, {
     method: 'GET',
-    headers: {
-      'Accept': 'application/vnd.github.v3+json'
-    }});
+    headers: REQUEST_HEADER
+  });
 
     /**
      * response.json() returns a promise so need await
