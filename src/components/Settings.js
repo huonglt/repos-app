eb9acf4ee6409cf4167930d867f44d670338d522
helen.thu.huong <>
2022-03-10 23:38:39 +1300

    show vacasa logo on Settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
+import "./repo.css";
+import logo from "../images/vacasa-logo.jpeg";
 
 const Settings = () => {
-  return (<div>Settings</div>);
-}
+  return (
+    <div className="settings-container">
+      <div>Settings</div>
+      <div className="settings">
+        <img src={logo} alt="vacasa-logo" className="settings-logo" />
+      </div>
+    </div>
+  );
+};
 
 export default Settings;
