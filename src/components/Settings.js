005134b58e83553fbdef9c389704250d0091d545
helen.thu.huong <>
2022-03-10 23:44:34 +1300

    styling Settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -7,7 +7,11 @@ const Settings = () => {
     <div className="settings-container">
       <div>Settings</div>
       <div className="settings">
-        <img src={logo} alt="vacasa-logo" className="settings-logo" />
+        <div className="settings-logo"><img src={logo} alt="vacasa-logo" className="settings-logo" /></div>
+        <div className="settings-block">
+          <div className="settings-text">Vacasa Interview</div>
+          <div className="settings-text">vacasa.interview@vacasa.com</div>
+        </div>
       </div>
     </div>
   );
