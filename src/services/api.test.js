a6892a9340a93fdce1b7a5f7e4625e4fcec0a39a
helen.thu.huong <>
2022-03-10 22:12:14 +1300

    tidy up unit test for getRepos method

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,14 +1,33 @@
-import { getRepos } from "./api.js";
+import { getRepos, QUERY_REPOS_URL } from "./api.js";
 
 describe("api module", () => {
+  // spy on jest
   beforeAll(() => jest.spyOn(global, "fetch"));
 
-  it("getRepos has url and header accept correctly", async () => {
+  it("getRepos has url, header accept and return data correctly", async () => {
     global.fetch.mockResolvedValueOnce({
       ok: true,
       json: async () => ({ success: true }),
     });
-    await getRepos();
+
+    // call getRepos method
+    const data = await getRepos();
+
+    // fetch been called
     expect(global.fetch).toHaveBeenCalledTimes(1);
+
+    // a get method with correct url, and Accept header
+    expect(global.fetch).toHaveBeenCalledWith(
+      QUERY_REPOS_URL,
+      expect.objectContaining({
+        method: "GET",
+        headers: {
+          Accept: "application/vnd.github.v3+json",
+        },
+      })
+    );
+
+    // data return correctly
+    expect(data).toEqual({ success: true });
   });
 });
