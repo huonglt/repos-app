066c9dc6e0cdb7b9efaa0eb70e11ff7a3997571c
helen.thu.huong <>
2022-03-10 22:47:41 +1300

    css to show list of repos as in sample design

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getRepos } from "../services/api";
 import Repo from "./Repo";
+import "./repo.css";
 
 const RepoList = () => {
   const [repos, setRepos] = useState([]);
@@ -15,11 +16,20 @@ const RepoList = () => {
   }, []);
 
   return (
-    <div>
-      <div>RepoList</div>
-      {repos.map((repo, index) => {
-        return <Repo name={repo.name} full_name={repo.full_name} description={repo.description} key={index} />;
-      })}
+    <div className="repos-container">
+      <div className="header">Repositories</div>
+      <div className="repos">
+        {repos.map((repo, index) => {
+          return (
+            <Repo
+              name={repo.name}
+              full_name={repo.full_name}
+              description={repo.description}
+              key={index}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
