a4c8aa0102cf2a4b769b90d1e93d966dae8a8034
helen.thu.huong <>
2022-03-11 21:10:33 +1300

    add retry to the hook to allow user to retry to call api when error occurs. This to avoid user having to press refresh button

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,31 +1,42 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react";
 
+/**
+ * Hook to call apiService
+ * Manage state of api call: is loading, is error, or loaded with data
+ * Also allow user to retry to call a api service if any error occurred
+ */
 export const useApi = (apiService) => {
-  const [ isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async() => {
-      setIsError(false);
-      setIsLoading(true);
-
-      try {
-        const result = await apiService();
-        if(result instanceof Error) {
-          setIsError(true);
-        } else {
-          setData(result);
-        }
-      } catch(err) {
+  /**
+   * load data. wrap in useCallback to avoid function re-created after each render
+   */
+  const loadData = useCallback(async () => {
+    setIsError(false);
+    setIsLoading(true);
+    try {
+      const result = await apiService();
+      if (result instanceof Error) {
         setIsError(true);
-      } finally {
-        setIsLoading(false);
+      } else {
+        setData(result);
       }
+    } catch (err) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
     }
-
-    fetchData();
   }, [apiService]);
 
-  return [isLoading, isError, data];
-}
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  /**
+   * allow user to retry if error occurs
+   */
+  const retry = loadData;
+  return [isLoading, isError, data, retry];
+};
