01dcd77bbcdaadd14976ccf09a42f2f0b9a64296
helen.thu.huong <>
2022-03-11 21:17:37 +1300

    Component to show Loading data, or a spinner etc

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/**
+ * Component to show Loading data, or a spinner etc
+ */
+const Loader = () => {
+  return <div>Loading data...</div>
+};
+
+export default Loader;
