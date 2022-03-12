8e257af520855bd6af6b51a31d1b254291d3ba0a
helen.thu.huong <>
2022-03-12 18:45:48 +1300

    unit test for Retry component

---
+++ src/tests/Retry.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Retry from "../components/Retry";
+
+describe("Retry component", () => {
+  it("Text and button Retry are correct", () => {
+    const retry = jest.fn();
+    render(<Retry retry={retry}/>);
+
+    // message Error while loading data on the document
+    expect(screen.getByText("Error while loading data")).toBeInTheDocument();
+
+    // click Retry button, retry function will be called
+    userEvent.click(screen.getByText("Retry"));
+    expect(retry).toHaveBeenCalledTimes(1);
+  })
+})
