04cacbf43005fc476987d9d843a18aec74a2c6bb
helen.thu.huong <>
2022-03-12 18:43:03 +1300

    remove unused import

--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { click } from "@testing-library/user-event/dist/click";
 import NavBar from "../components/NavBar";
 
 describe("NavBar component", () => {
