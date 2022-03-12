1b0a4eef9863e9ad96a8450adc8478c4c83c8ee0
helen.thu.huong <>
2022-03-12 18:17:57 +1300

    add data-testid for unit testing

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -11,7 +11,7 @@ const Settings = () => {
     <div className="settings-container">
       <div className="header">Settings</div>
       <div className="settings">
-        <div className="settings-logo"><img src={logo} alt="vacasa-logo" className="settings-logo" /></div>
+        <div className="settings-logo"><img src={logo} alt="vacasa-logo" className="settings-logo" data-testid="vacasa-logo"/></div>
         <div className="settings-block">
           <div className="settings-text">Vacasa Interview</div>
           <div className="settings-text">vacasa.interview@vacasa.com</div>
