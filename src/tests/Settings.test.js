24f84c41e25e278d4389c4a95975c4f4575ae9f7
helen.thu.huong <>
2022-03-12 18:18:05 +1300

    unit test for Settings component

---
+++ src/tests/Settings.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Settings from "../components/Settings";
+
+describe("Settings component", () => {
+  it("Texts are correct, vacasa logo is shown", () => {
+    render(<Settings/>);
+    expect(screen.getByText("Settings")).toBeInTheDocument();
+    expect(screen.getByTestId("vacasa-logo")).toBeInTheDocument();
+    expect(screen.getByText("Vacasa Interview")).toBeInTheDocument();
+    expect(screen.getByText("vacasa.interview@vacasa.com")).toBeInTheDocument();
+    expect(screen.getByText("Support")).toBeInTheDocument();
+    expect(screen.getByText("Privacy Policy")).toBeInTheDocument();
+    expect(screen.getByText("Terms of Service")).toBeInTheDocument();
+  });
+})
