07d52e588cbf7c5b47aede78b2a170aec442c90f
helen.thu.huong <>
2022-03-12 18:49:56 +1300

    styling loader & retry

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './components.css';
 
 /**
  * Component to show Loading data, or a spinner etc
  */
 const Loader = () => {
-  return <div>Loading data...</div>
+  return <div className="loader">Loading data...</div>
 };
 
 export default Loader;
--- src/components/Retry.js
+++ src/components/Retry.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './components.css';
 
 /**
  * Component to show error message when fail to load data from api service.
  * Show a Retry button to try again
  */
 const Retry = ({retry}) => {
-  return <div>Error while loading data <button onClick={() => retry()}>Retry</button></div> 
+  return <div className="retry">Error while loading data <button onClick={() => retry()}>Retry</button></div> 
 };
 
 export default Retry;
