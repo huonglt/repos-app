65d7f1085a298ed4740dffb7259f1880b72dab9d
helen.thu.huong <>
2022-03-12 18:41:48 +1300

    unit test Click Settings nav item, Settings page shown correctly

--- src/tests/Main.test.js
+++ src/tests/Main.test.js
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import Main from "../components/Main";
 import { getRepos } from "../apis/api";
+import userEvent from "@testing-library/user-event";
 
 jest.mock("../apis/api");
 
 
 describe("Main component", () => {
-  it("Repositories page shown initially", async () => {
+  beforeEach(() => {
     const mockRepos = [
       { name: 'repo-app', full_name: 'huonglt/repo-app', description: 'list all repos on github of an organization'}
     ];
@@ -18,6 +19,9 @@ describe("Main component", () => {
         }, 10);
       });
     });
+  });
+
+  it("Repositories page shown initially", async () => {
     render(<Main/>);
 
     // Exist 2 Repositories text in the document
@@ -34,4 +38,22 @@ describe("Main component", () => {
     let description = await screen.findByText("list all repos on github of an organization");
     expect(description).toBeInTheDocument();
   });
+
+  it("Click Settings nav item, Settings page shown correctly", () => {
+    render(<Main/>);
+
+    // click Settings nav item
+    userEvent.click(screen.getByText("Settings"));
+
+    // 2 text Settings found
+    expect(screen.getAllByText("Settings").length).toEqual(2);
+
+    // rest of ui of Settings page
+    expect(screen.getByTestId("vacasa-logo")).toBeInTheDocument();
+    expect(screen.getByText("Vacasa Interview")).toBeInTheDocument();
+    expect(screen.getByText("vacasa.interview@vacasa.com")).toBeInTheDocument();
+    expect(screen.getByText("Support")).toBeInTheDocument();
+    expect(screen.getByText("Privacy Policy")).toBeInTheDocument();
+    expect(screen.getByText("Terms of Service")).toBeInTheDocument();
+  })
 });
